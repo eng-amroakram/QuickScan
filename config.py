a7615e6390e75050161cf1004dd6efef5fba1cef
eng-amroakram <eng.amroakram@gmail.com>
2024-11-13 16:59:17 +0200

    new udpates

--- config.py
+++ config.py
@@ -10,7 +10,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.path.join(BASE_DIR, "QuickScan.db")
 
 # Upload folder configuration
-UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")  # Define the path for the upload folder
+UPLOAD_FOLDER = os.path.join(
+    BASE_DIR, "uploads"
+)  # Define the path for the upload folder
 YARA_DIR = os.path.join(BASE_DIR, "app", "yara")  # Directory containing YARA rules
 
 # Flask-Mail configuration
